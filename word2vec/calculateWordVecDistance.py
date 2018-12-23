9bbc64721a83668b139c8b40988202e61ab5d02e
gavin <f106524018@g.ncu.edu.tw>
2018-12-24 03:01:44 +0800

    update

--- word2vec/calculateWordVecDistance.py
+++ word2vec/calculateWordVecDistance.py
@@ -2,15 +2,11 @@
 #!usr/bin/env python
 import json
 
-Json_file = open("./output.txt","r")
-Json_file = json.load(Json_file)
-'''
-print(d)
-print(d[u'\u5c07\u4e4b\u652c'])
-print(u'\u5c07\u4e4b\u652c')
-'''
+
+
 def location(text):
-    uni_text=unicode(text,"utf-8")
+    #uni_text=unicode(text,"utf-8")
+    uni_text=text
     try:
         #print(Json_file[uni_text])
         return Json_file[uni_text]
@@ -18,16 +14,32 @@ def location(text):
         #print(uni_text)
         return
 def Distance(t1,t2):
-    x = t1[0]-t2[0]
-    y = t1[1]-t2[1]
-    return ((x**2+y**2)**0.5)*10000000000
+    assert len(t1) == len(t2)
+    sum=0
+    for i in range(len(t1)):
+        sum+=((t1[i]-t2[i])**2)
+
+
+    #x = t1[0]-t2[0]
+    #y = t1[1]-t2[1]
+    #return ((x**2+y**2)**0.5)*10000000000
+    return (sum**0.5)*100
 
 def Distance_word(w1,w2):
     print(w1+','+w2)
     print(Distance(location(w1),location(w2)))
 
 
+print('loading json Data .......')
+Json_file = open("./output/outputWord2Vec(v300)(50000).txt","r")
+Json_file = json.load(Json_file)
+'''
+print(d)
+print(d[u'\u5c07\u4e4b\u652c'])
+print(u'\u5c07\u4e4b\u652c')
+'''
 
+print('calculate distance .......')
 
 Distance_word("害怕","恐懼")
 Distance_word("害怕","畏懼")
